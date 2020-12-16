5716670c6f25a7bf56cd915b1328dadddfaf9761
Tony Lai <a30942@gmail.com>
2020-12-16 15:41:07 +0800

    Update ex_Online_AC.py

--- src/ex_Online_AC.py
+++ src/ex_Online_AC.py
@@ -33,6 +33,74 @@ def Online_AC(flow, prev_flows, topo):
 
 ################### End here ###########################
 
+
+#################### [NOTICE] ############################
+#            Don't modify this part
+########### Evaluate Admission Control Decision ##########
+#              Evaluation rule
+#       Link allocated rate = bandwidth / # of flow remain on this link
+#       Flow Allocated Rate = Bottleneck link allocated rate of the flow
+#       Allocated Rate > 5 * 80% -> "1"
+#       Allocated Rate < 5 * 80% -> "-2"
+##########################################################
+def Evaluate(flow_sequence, admission_control):
+  flow_score = []
+  flow_remained = []
+  for idx, flow in enumerate(flow_sequence):
+  
+    flow_score.append([flow[0], flow[1], flow[2], 0])
+    
+    if admission_control[idx]==1:
+      
+      ## Previous flow leave
+      cur_time = flow[2]
+      while flow_remained:
+        if  cur_time - flow_remained[0][3]  < DURATION:
+          break
+        flow_remained.pop(0) 
+
+      flow_remained.append([idx, flow[0], flow[1], flow[2]])
+      flow_score[idx][3] = 1
+
+      # Check flows number on each link        
+      links = np.zeros((11, 11))
+      for f in flow_remained:
+        path_id = f[1]-1
+        path = paths[path_id]
+        for node, next_node in zip(path, path[1:]):
+          links[node][next_node] +=1
+          links[next_node][node] +=1
+
+      # Store flow  allocated rate < 5 * 80% 
+      minus_seq_id = []
+      for f in flow_remained:
+        path_id = f[1]-1
+        path = paths[path_id]
+        for node, next_node in zip(path, path[1:]):
+          if links[node][next_node] > 2 or links[next_node][node] > 2:
+            minus_seq_id.append(f[0])
+            break
+
+
+      ## Update score for flow stay in the network
+      for seq_id in minus_seq_id:
+        flow_score[seq_id][3] = -2
+      #print links
+
+  total_score = 0
+  print '\n#########################################################'
+  print '###############  Score of each flow  ######################'
+  print '#########################################################\n'
+  for idx, f in enumerate(flow_score):
+    if idx >= 20:
+      print ("Flow ({}, {}, {}) score: {}".format(f[0], f[1], f[2], f[3]))
+      total_score += f[3]
+  print '\n######################################################################'
+  print '##########  Result of Online Admission Control Scheduling ############'
+  print '######################################################################\n'
+  print ("Total score of Online AC is {}".format(total_score))
+  ################### End here ###########################  
+
 if __name__=="__main__":
 
   ################## [NOTICE] ############################
@@ -91,72 +159,8 @@ if __name__=="__main__":
       prev_flows.append(flow)
   ###################### End here ###########################
 
+  #################### Evaluate  ###########################
+  Evaluate(flow_sequence, admission_control)
+  ###################### End here ###########################
 
-  #################### [NOTICE] ############################
-  #            Don't modify this part
-  ########### Evaluate Admission Control Decision ##########
-  #              Evaluation rule
-  #       Link allocated rate = bandwidth / # of flow remain on this link
-  #       Flow Allocated Rate = Bottleneck link allocated rate of the flow
-  #       Allocated Rate > 5 * 80% -> "1"
-  #       Allocated Rate < 5 * 80% -> "-2"
-  ##########################################################
-  flow_score = []
-  flow_remained = []
-  for idx, flow in enumerate(flow_sequence):
-  
-    flow_score.append([flow[0], flow[1], flow[2], 0])
-    
-    if admission_control[idx]==1:
-      
-      ## Previous flow leave
-      cur_time = flow[2]
-      while flow_remained:
-        if  cur_time - flow_remained[0][3]  < DURATION:
-          break
-        flow_remained.pop(0) 
-
-      flow_remained.append([idx, flow[0], flow[1], flow[2]])
-      flow_score[idx][3] = 1
-
-      # Check flows number on each link        
-      links = np.zeros((11, 11))
-      for f in flow_remained:
-        path_id = f[1]-1
-        path = paths[path_id]
-        for node, next_node in zip(path, path[1:]):
-          links[node][next_node] +=1
-          links[next_node][node] +=1
-
-      # Store flow  allocated rate < 5 * 80% 
-      minus_seq_id = []
-      for f in flow_remained:
-        path_id = f[1]-1
-        path = paths[path_id]
-        for node, next_node in zip(path, path[1:]):
-          if links[node][next_node] > 2 or links[next_node][node] > 2:
-            minus_seq_id.append(f[0])
-            break
-
-
-      ## Update score for flow stay in the network
-      for seq_id in minus_seq_id:
-        flow_score[seq_id][3] = -2
-      #print links
-
-  total_score = 0
-  print '\n#########################################################'
-  print '###############  Score of each flow  ######################'
-  print '#########################################################\n'
-  for idx, f in enumerate(flow_score):
-    if idx >= 20:
-      print ("Flow ({}, {}, {}) score: {}".format(f[0], f[1], f[2], f[3]))
-      total_score += f[3]
-  print '\n######################################################################'
-  print '##########  Result of Online Admission Control Scheduling ############'
-  print '######################################################################\n'
-  print ("Total score of Online AC is {}".format(total_score))
-
-  
-  ################### End here ###########################  
   
